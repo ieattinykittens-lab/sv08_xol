62313e93ee9ecbcb174ab2f9960cbe7df2345b48
biqu <biqu@SV08-0841e61>
2025-10-21 09:41:51 +0000

    index.lock modified - 10/21/2025 - 09:41:32 AM

--- klipper/out/src/rp2040/rpxxxx_link.d
+++
@@ -1,2 +0,0 @@
-out/src/rp2040/rpxxxx_link.ld: src/rp2040/rpxxxx_link.lds.S \
- out/autoconf.h
--- klipper/out/src/spi_software.d
+++
@@ -1,6 +0,0 @@
-out/src/spi_software.o: src/spi_software.c out/autoconf.h \
- out/board/gpio.h /usr/lib/gcc/arm-none-eabi/8.3.1/include/stdint.h \
- out/board-generic/board/misc.h \
- /usr/lib/gcc/arm-none-eabi/8.3.1/include/stdarg.h src/basecmd.h \
- /usr/lib/gcc/arm-none-eabi/8.3.1/include/stddef.h src/command.h \
- src/ctr.h src/compiler.h src/sched.h src/spicmds.h
