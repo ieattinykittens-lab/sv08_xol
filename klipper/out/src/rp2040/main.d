a010d7be72549c89602cb3c2fd18eb9f7407a609
biqu <biqu@SV08-0841e61>
2025-10-22 01:35:02 +0000

    index.lock modified - 10/22/2025 - 01:17:53 AM

---
+++ klipper/out/src/rp2040/main.d
@@ -0,0 +1,25 @@
+out/src/rp2040/main.o: src/rp2040/main.c \
+ /usr/lib/gcc/arm-none-eabi/8.3.1/include/stdint.h \
+ out/board-generic/board/misc.h \
+ /usr/lib/gcc/arm-none-eabi/8.3.1/include/stdarg.h \
+ src/generic/armcm_reset.h lib/pico-sdk/rp2040/hardware/structs/clocks.h \
+ lib/pico-sdk/hardware/address_mapped.h \
+ lib/pico-sdk/rp2040/hardware/regs/addressmap.h \
+ lib/pico-sdk/rp2040/hardware/platform_defs.h \
+ lib/pico-sdk/rp2040/hardware/regs/clocks.h \
+ lib/pico-sdk/rp2040/hardware/structs/pll.h \
+ lib/pico-sdk/rp2040/hardware/regs/pll.h \
+ lib/pico-sdk/rp2040/hardware/structs/resets.h \
+ lib/pico-sdk/rp2040/hardware/regs/resets.h \
+ lib/pico-sdk/rp2040/hardware/structs/watchdog.h \
+ lib/pico-sdk/rp2040/hardware/regs/watchdog.h \
+ lib/pico-sdk/rp2040/hardware/structs/xosc.h \
+ lib/pico-sdk/rp2040/hardware/regs/xosc.h src/rp2040/internal.h \
+ out/autoconf.h lib/pico-sdk/rp2040/cmsis_include/RP2040.h \
+ lib/cmsis-core/core_cm0plus.h lib/cmsis-core/cmsis_version.h \
+ lib/cmsis-core/cmsis_compiler.h lib/cmsis-core/cmsis_gcc.h \
+ lib/cmsis-core/mpu_armv7.h \
+ lib/pico-sdk/rp2040/cmsis_include/system_RP2040.h src/sched.h src/ctr.h \
+ src/compiler.h \
+ lib/pico-sdk/rp2040/hardware/structs/vreg_and_chip_reset.h \
+ lib/pico-sdk/rp2040/hardware/regs/vreg_and_chip_reset.h
---
+++ klipper/out/src/rp2040/rpxxxx_link.d
@@ -0,0 +1,2 @@
+out/src/rp2040/rpxxxx_link.ld: src/rp2040/rpxxxx_link.lds.S \
+ out/autoconf.h
