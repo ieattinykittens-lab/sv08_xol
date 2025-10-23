cc7dc6c66f2f71233cdd43c093eaab3f6331702e
biqu <biqu@SV08-0841e61>
2025-10-23 08:29:48 +0000

    index.lock modified - 10/23/2025 - 07:43:26 AM

---
+++ klipper/out/src/rp2040/chipid.d
@@ -0,0 +1,9 @@
+out/src/rp2040/chipid.o: src/rp2040/chipid.c out/autoconf.h \
+ out/board-generic/board/canserial.h \
+ /usr/lib/gcc/arm-none-eabi/8.3.1/include/stdint.h src/generic/usb_cdc.h \
+ src/generic/usbstd.h src/compiler.h src/rp2040/internal.h \
+ lib/pico-sdk/rp2040/cmsis_include/RP2040.h lib/cmsis-core/core_cm0plus.h \
+ lib/cmsis-core/cmsis_version.h lib/cmsis-core/cmsis_compiler.h \
+ lib/cmsis-core/cmsis_gcc.h lib/cmsis-core/mpu_armv7.h \
+ lib/pico-sdk/rp2040/cmsis_include/system_RP2040.h src/sched.h src/ctr.h \
+ src/compiler.h
---
+++ klipper/out/src/rp2040/watchdog.d
@@ -0,0 +1,10 @@
+out/src/rp2040/watchdog.o: src/rp2040/watchdog.c \
+ /usr/lib/gcc/arm-none-eabi/8.3.1/include/stdint.h \
+ lib/pico-sdk/rp2040/hardware/structs/psm.h \
+ lib/pico-sdk/hardware/address_mapped.h \
+ lib/pico-sdk/rp2040/hardware/regs/addressmap.h \
+ lib/pico-sdk/rp2040/hardware/platform_defs.h \
+ lib/pico-sdk/rp2040/hardware/regs/psm.h \
+ lib/pico-sdk/rp2040/hardware/structs/watchdog.h \
+ lib/pico-sdk/rp2040/hardware/regs/watchdog.h src/sched.h src/ctr.h \
+ src/compiler.h
