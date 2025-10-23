ad33c7c619729489dc3913d29b61df47c87ffdac
biqu <biqu@SV08-0841e61>
2025-10-23 21:42:23 +0000

    index.lock modified - 10/23/2025 - 09:41:37 PM

---
+++ klipper/out/src/rp2040/bootrom.d
@@ -0,0 +1,26 @@
+out/src/rp2040/bootrom.o: src/rp2040/bootrom.c \
+ /usr/lib/gcc/arm-none-eabi/8.3.1/include/stdint.h \
+ /usr/include/newlib/string.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/sys/reent.h \
+ /usr/include/newlib/_ansi.h \
+ /usr/lib/gcc/arm-none-eabi/8.3.1/include/stddef.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/sys/_locale.h /usr/include/newlib/strings.h \
+ /usr/include/newlib/sys/string.h out/board-generic/board/irq.h \
+ src/compiler.h lib/pico-sdk/rp2040/hardware/structs/ioqspi.h \
+ lib/pico-sdk/rp2040/hardware/structs/io_qspi.h \
+ lib/pico-sdk/hardware/address_mapped.h \
+ lib/pico-sdk/rp2040/hardware/regs/addressmap.h \
+ lib/pico-sdk/rp2040/hardware/platform_defs.h \
+ lib/pico-sdk/rp2040/hardware/regs/io_qspi.h \
+ lib/pico-sdk/rp2040/hardware/structs/ssi.h \
+ lib/pico-sdk/rp2040/hardware/regs/ssi.h src/rp2040/internal.h \
+ out/autoconf.h lib/pico-sdk/rp2040/cmsis_include/RP2040.h \
+ lib/cmsis-core/core_cm0plus.h lib/cmsis-core/cmsis_version.h \
+ lib/cmsis-core/cmsis_compiler.h lib/cmsis-core/cmsis_gcc.h \
+ lib/cmsis-core/mpu_armv7.h \
+ lib/pico-sdk/rp2040/cmsis_include/system_RP2040.h
